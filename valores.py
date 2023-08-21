28a8d230cd94770268145881dadf7ee03d6f0aee
celso eduardo <celsoeduardobentopugim@gmail.com>
2023-08-21 20:04:41 -0300

    valores

---
+++ valores.py
@@ -0,0 +1,10 @@
+print ("calculadora de valores")
+
+valor1 = float (input ("digite um valor "))
+valor2 = float (input("digite outro valçor "))
+
+if valor1>valor2:
+    print("o primeiro valor e mais alto que o segundo")
+else:
+    valor1<valor2
+    print("o segundo valor e mais alto que o primeiro")
